400e6755bfe8c2245f99be54284214cdc782c712
hye219 <jhy_0219@daum.net>
2022-04-22 17:01:03 +0900

    #1

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -25,4 +25,27 @@ public class UserDao {
         connection.close();
         return user;
     }
+
+    public void insert(User user) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost/userdao2?serverTimezone=UTC"
+                        , "1234", "1234");
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into userdao2 (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+
+        //User user = new User();
+        user.setId(resultSet.getInt(1));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        //return user;
+    }
 }
